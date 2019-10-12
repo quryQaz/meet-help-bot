51620b99127c45760f339275e71caeac0feb6fe4
vlad qaz <vladqaz111@ya.ru>
2019-10-12 23:38:50 +0300

    Initial Commit

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from telegram.ext import MessageHandler
 from telegram.ext import Filters
 import random
 
-from config import TG_TOKEN
-from config import TG_API_URL
+TG_TOKEN = "798290356:AAFbCMlWoqeXIlWg7pw1N4CpSWt7roMwI4s"
+TG_API_URL = "https://telegg.ru/orig/bot"
 
 
 
